fceb2ef00509f4160fcf56a1a79cb1d2417f3912
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-25 17:41:52 +0000

    Use built in aside HTML element

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar({
   onSelectProject,
 }) {
   return (
-    <section className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         YOUR PROJECTS
       </h2>
@@ -22,6 +22,6 @@ export default function Sidebar({
         projects={projects}
         onSelect={onSelectProject}
       />
-    </section>
+    </aside>
   );
 }
