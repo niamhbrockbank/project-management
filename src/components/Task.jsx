10310ff11e5d1a3f7f5e1e8adde509262a047a6e
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-26 18:05:56 +0000

    Style clear task button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,10 @@ export default function Task({ task, index, onDelete }) {
   return (
     <div className="flex justify-between w-full px-4 py-2 rounded-md bg-stone-100">
       <p className="bg-inherit">{task}</p>
-      <button onClick={() => onDelete(index)} className="bg-inherit font-light">
+      <button
+        onClick={() => onDelete(index)}
+        className="bg-inherit font-light hover:text-red-700"
+      >
         Clear
       </button>
     </div>
