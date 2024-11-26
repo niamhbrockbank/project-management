789aa5376375e1016973155aa19958128ed81d2e
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-26 14:44:22 +0000

    Handle no projects outside of project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,3 @@
-import NoProjectSelected from "./NoProjectSelected";
 import ProjectSummary from "./ProjectSummary";
 import Tasks from "./Tasks";
 
@@ -10,21 +9,19 @@ export default function Project({
 }) {
   return (
     <>
-      {!project && <NoProjectSelected />}
-      {project && (
-        <section id="project" className="w-full px-11">
-          <ProjectSummary
-            project={project}
-            onDelete={onDelete}
-            onEdit={onEditProject}
-          />
-          <Tasks
-            tasks={project.tasks}
-            onEdit={onEditProject}
-            onDelete={onDeleteTask}
-          />
-        </section>
-      )}
+      <section id="project" className="w-full px-11">
+        <ProjectSummary
+          project={project}
+          onDelete={onDelete}
+          onEdit={onEditProject}
+        />
+        <Tasks
+          tasks={project.tasks}
+          onEdit={onEditProject}
+          onDelete={onDeleteTask}
+        />
+      </section>
+      )
     </>
   );
 }
